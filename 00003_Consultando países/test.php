a3c96418b3d3e147448c32c962a4d439fb6d1c65
thomaz@digitalhouse.com <thomaz@digitalhouse.com>
2019-08-14 19:40:42 +0000

    Mumuki Export on 2019-08-14 19:40:42 +0000

--- 00003_Consultando países/test.php
+++ 00003_Consultando países/test.php
@@ -1,3 +1,8 @@
-public function testDescriptionExample(): void {
-  $this->assertTrue(true);
-}
+  public function testCurl(): void {
+    $controller = new PaisesController();
+    $controller->listar();
+
+    $this->assertTrue(Curl::$to === "https://restcountries.eu/rest/v2/all", "Você utilizou Curl::to? e colocou a URL correta?");
+    
+    $this->assertTrue(Curl::$get, "Chamou o método get?");
+  }
